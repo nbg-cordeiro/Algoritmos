4358658e4bcc39bee045fa812a09422bd7e3d00b
Cordeiro <nbg.cordeiro@gmail.com>
2026-05-24 13:27:05 -0300

    Add Codeforces problems

---
+++ Codeforces/Rating 800/Domino_piling.cpp
@@ -0,0 +1,12 @@
+// enunciado:
+// https://codeforces.com/problemset/problem/50/A
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    int hor, ver;
+    cin >> hor >> ver;
+    cout << (hor * ver)/2 << endl;
+    return 0;
+}
